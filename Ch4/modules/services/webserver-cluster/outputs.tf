b637c57927ec3e89411a41d645999bdb636826c2
Dustin <dshargool2009@gmail.com>
2021-03-23 14:57:00 -0600

    fix names in modules

--- Ch4/modules/services/webserver-cluster/outputs.tf
+++ Ch4/modules/services/webserver-cluster/outputs.tf
@@ -7,7 +7,3 @@ output "asg_name" {
   value       = aws_autoscaling_group.example.name
   description = "The name of the Auto Scaling Group"
 }
-output "alb_dns_name" {
-  value       = aws_lb.example.alb_dns_name
-  description = "The DNS name of the load balancer"
-}
